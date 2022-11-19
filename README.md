633e1fe10e06993e2278da6b249160deb5cc814f
mario piccinelli <mario.piccinelli@gmail.com>
2022-11-19 23:05:10 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Endpoint HTTP per splittare un documento a partire dai barcode sulle pagine. Esperimento di applicazione Docker su SAP Cloud Foundry.
+
+Crea un endpoint POST che riceve un PDF come payload. Individua la presenza di barcode che seguono un determinato pattern, e li utilizza come indicatori di inizio delle parti in cui è diviso il documento. Divide il documento in singoli PDF e li restituisce al chiamante sotto forma di multipart.
+
+## Creazione immagine Docker
+
+- Richiede Docker sulla macchina locale.
+
+- Creare account su [Docker Hub](https://hub.docker.com/).
+
+- Creare immagine docker (nota: come prefisso del tag usare il proprio nome account su Docker Hub).
+
+	`docker build -t piccimario/barcode-split-cf .`
+
+- Test in locale (viene esposta la porta 3333). L'opzione `--rm` serve a eliminare l'istanza (container) di test quando viene fermata. Le opzioni `-t` e `-i` sono utili durante i test perchè forzano l'allocazione di un terminale e lasciano il container in modalità interattiva, il che ci permette di terminarlo usando Ctrl+C.
+
+	`docker run -t -i --rm -p 3333:3333 piccimario/barcode-split-cf`
+
+- Push su Docker Hub.
+
+	`docker push piccimario/barcode-split-cf`
+
+## Caricamento immagine su Cloud Foundry:
+
+- Creare [account trial](https://account.hanatrial.ondemand.com/trial/#/home/trial).
+- Installare [tools cli](https://github.com/cloudfoundry/cli/releases) per Cloud Foundry.
+- `cf api https://api.cf.eu20.hana.ondemand.com` (usare endpoint cf corretto per il proprio trial)
+- `cf login`
+- `cf push barcode-docker --docker-image piccimario/barcode-split-cf --docker-username piccimario -k 512M -m 512M`
+
+---
+
+## Risorse varie:
+
+- https://adamtheautomator.com/flask-web-server/
+
+- https://blogs.sap.com/2021/01/02/deploy-python-application-on-sap-cloud-platform-using-docker-container/
+
+- https://techtutorialsx.com/2020/01/01/python-pyzbar-detecting-and-decoding-barcode/
+
+- https://barcode.tec-it.com (servizio gratuito per generare immagini di barcode in diversi formati)
---
+++ barcode.py
@@ -0,0 +1,92 @@
+import os, json, re, io
+from flask import Flask, request, Response
+from pyzbar import pyzbar
+import pdf2image
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from cfenv import AppEnv
+from requests_toolbelt import MultipartEncoder
+
+env = AppEnv()
+
+app = Flask(__name__)
+port = 3333
+
+def calc_split_positions(pages, pattern=None, barcode_type='CODE128'):
+
+	"""
+	Calculates split positions by looking for barcodes which follow the specified pattern (if provided).
+	Returns a list of tuples (page number, barcode value).
+	"""
+	
+	split_positions = []
+
+	for index, page in enumerate(pages):
+		found_barcodes = pyzbar.decode(page)
+		for obj in found_barcodes:
+			
+			# print(f"Barcode {obj.data.decode('utf-8')} (type {obj.type}) found on page {index}")
+			
+			if (obj.type != barcode_type): continue
+			if (pattern and not pattern.match(obj.data.decode('utf-8'))): continue
+			
+			split_positions.append((index, obj.data.decode('utf-8')))
+			
+				
+	return split_positions
+
+
+@app.route('/barcodesplit', methods=['POST'])
+def barcodesplit():
+
+	barcode_pattern = '^DOC[0-9]+$'
+	barcode_compiled_pattern = re.compile(barcode_pattern)
+
+	pages = pdf2image.convert_from_bytes(
+		request.get_data(),
+		dpi=400,
+		grayscale=True
+	)
+
+	# Calculates split positions (page numbers)
+
+	split_positions = calc_split_positions(
+		pages, 
+		pattern=barcode_compiled_pattern,
+		barcode_type='CODE128'
+	)
+
+	# Split files and build return payload
+
+	inputpdf = PdfFileReader(io.BytesIO(request.get_data()))
+
+	response_data = {}
+
+	for i, element in enumerate(split_positions):
+		
+		(first_page, barcode) = element
+		
+		if i < len(split_positions)-1:
+			last_page = split_positions[i+1][0]
+		else:
+			# For the last split position, the last page
+			# is the last page of the document.
+			last_page = inputpdf.numPages
+					
+		output = PdfFileWriter()
+		
+		for page in range(first_page, last_page):
+			output.addPage(inputpdf.getPage(page))
+
+		tmp = io.BytesIO()
+		output.write(tmp)
+
+		response_data[f"part-{i}"] = (f"{i:03}-{barcode}.pdf", tmp.getvalue(), 'application/pdf')
+
+	# Send return payload
+
+	m = MultipartEncoder(response_data)
+	return Response(m.to_string(), mimetype=m.content_type)	
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
